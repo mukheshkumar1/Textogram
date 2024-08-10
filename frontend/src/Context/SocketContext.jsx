871a690496560746762cef59301d0b69720e864f
mukheshkumar1 <mukheshkumar8747@gmail.com>
2024-08-10 10:44:55 +0530

    socket url

--- frontend/src/Context/SocketContext.jsx
+++ frontend/src/Context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://textogram-lo7b.onrender.com/", {
+			const socket = io("https://textogram-lo7b.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
